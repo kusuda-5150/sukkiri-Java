276a71caf8041d0d997185605eb0b696b31e4ccd
kusuda-5150 <kusuda925@gmail.com>
2026-04-17 07:21:02 +0900

    変数cの定義を忘れた。

---
+++ src/ch01/p57/Pr_1_2.java
@@ -0,0 +1,9 @@
+package ch01.p57;
+
+public class Pr_1_2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + (a * b));
+    }
+}
