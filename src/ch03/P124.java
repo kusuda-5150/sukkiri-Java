d33dac86bbd2e707e856da5746a027c642d16344
kusuda-5150 <kusuda925@gmail.com>
2026-04-20 23:16:47 +0900

    //

---
+++ src/ch03/P124.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class P124 {
+    public static void main (String[] args) {
+        int temp = 24;
+        do {
+         temp--;
+            System.out.println("温度を1度下げました");
+        } while(temp > 25);
+    }
+}
---
+++ src/ch03/P125.java
@@ -0,0 +1,9 @@
+package ch03;
+
+public class P125 {
+    public static void main (String[] args) {
+        for (int i =0; i < 10; i++) {
+            System.out.println("こんにちは");
+        }
+    }
+}
