af942575143915d5ee0f3147a86a72e28574a526
kusuda-5150 <kusuda925@gmail.com>
2026-04-23 06:19:01 +0900

    //

---
+++ src/ch04/P153.java
@@ -0,0 +1,15 @@
+
+package ch04;
+
+public class P153 {
+    public static void main(String[] args) {
+        int[] scores = {20, 30, 40, 50, 80};
+        int count = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] >= 50) {
+                count++;
+            }
+        }
+        System.out.println("50点以上の科目は：" + count);
+    }
+}
---
+++ src/ch04/P157.java
@@ -0,0 +1,11 @@
+
+package ch04;
+
+public class P157 {
+    public static void main(String[] args) {
+        int[] scores = {20, 30, 40, 50, 80};
+        for (int value: scores) {
+            System.out.println(value);
+        }
+    }
+}
